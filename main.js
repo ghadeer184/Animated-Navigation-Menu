fcd8017b71b29532b8f65c4d4f1b68690efc8f65
Ghadeer <ghadeer@Ghadeers-MacBook-Pro.local>
2022-01-03 00:55:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+const list = document.querySelectorAll('.list');
+const indicator = document.getElementById('indicator');
+
+list.forEach((el, idx) => {
+    el.addEventListener('click', () => {
+        removeActive();
+        el.classList.add('active');
+        indicator.style.left = `${idx * 70}px`;
+    });
+})
+
+function removeActive() {
+    list.forEach(el => {
+        el.classList.remove('active');
+    })
+}
